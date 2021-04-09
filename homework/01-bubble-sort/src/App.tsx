e23ee061e83e17d35fd563e834132362851c177f
Dzmitry Bryt <bdv212@yandex.ru>
2021-04-09 13:58:08 +0300

    feat: init project

---
+++ homework/01-bubble-sort/src/App.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return <div className='App'>01-bubble-sort</div>;
+  }
+}
+
+export default App;
